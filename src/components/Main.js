cbea020bf0161a5ae1c6d12de1821f32d5791a5d
bpriyabhashana <bpriyabhashana@gmail.com>
2022-02-10 16:03:56 +0530

    Menu updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import {
   Grid,
   Typography,
   Avatar,
-  Popover,
+  Menu,
   MenuList,
   MenuItem,
   ListItemAvatar,
@@ -175,7 +175,7 @@ const Main = (props) => {
                     </Avatar>
                   )}
                 </Grid>
-                <Popover
+                <Menu
                   id={id}
                   open={openPopover}
                   anchorEl={anchorEl}
@@ -187,6 +187,33 @@ const Main = (props) => {
                   transformOrigin={{
                     vertical: "top",
                     horizontal: "right",
+                  }}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      width: 300,
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                      mt: 1.5,
+                      "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      "&:before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                      },
+                    },
                   }}
                 >
                   <MenuList>
@@ -225,7 +252,7 @@ const Main = (props) => {
                       <ListItemText primary="Logout" />
                     </MenuItem>
                   </MenuList>
-                </Popover>
+                </Menu>
               </Grid>
             </Grid>
           </Grid>
